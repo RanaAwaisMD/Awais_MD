f0698b1cc36931171a9b794c0f04c2b6cdee6c2c
RanaAwaisMD <ra36197714@gmail.com>
2025-10-03 02:25:49 +0500

    Create help.js

---
+++ commands/help.js
@@ -0,0 +1,165 @@
+
+╭─✦〔 *𝚀𝚄𝙴𝙴𝙽 𝚁𝙸𝙰𝙼* 〕
+│ 👤 ᴏᴡɴᴇʀ     : Hector Manuel
+│ 🌍 ᴍᴏᴅᴇ      : [ public ]
+│ ⏰ ᴛɪᴍᴇ      : 00:25:04
+│ 📅 ᴅᴀᴛᴇ      : 10/02/2025
+│ 🛠️ ᴘʀᴇғɪx    : [ . ]
+│ 🔄 ᴜᴘᴛɪᴍᴇ    : 3h 21m 30s
+│ 🌐 ᴛɪᴍᴇᴢᴏɴᴇ: Africa/Accra
+│ 🚀 ᴠᴇʀsɪᴏɴ   : 1.0.0
+╰─⭘━━━━━━━━━━⚬─╯
+
+╭─✦〔 🤖 *AI Menu* 〕
+│ ➸ .gpt 
+│ ➸ .gemini 
+│ ➸ .deepseek
+│ ➸ .flux 
+╰─✦
+
+╭─✦〔 📥 *Download* 〕
+│ ➸ .play 
+│ ➸ .song 
+│ ➸ .instagram 
+│ ➸ .facebook 
+│ ➸ .tiktok 
+│ ➸ .video 
+│ ➸ .ytmp4 
+│ ➸ .xnxx
+╰─✦
+
+╭─✦〔 🎯 *Fun Menu* 〕
+│ ➸ .compliment 
+│ ➸ .insult 
+│ ➸ .flirt
+│ ➸ .goodnight
+│ ➸ .character 
+│ ➸ .crush
+│ ➸ .simp 
+│ ➸ .stupid 
+╰─✦
+
+╭─✦〔 🎮 *Games* 〕
+│ ➸ .tictactoe 
+│ ➸ .hangman
+│ ➸ .guess 
+│ ➸ .trivia
+│ ➸ .answer 
+│ ➸ .truth
+│ ➸ .dare
+╰─✦
+
+╭─✦〔 *Group Menu* 〕
+│ ➸ .ban 
+│ ➸ .promote 
+│ ➸ .demote 
+│ ➸ .open
+│ ➸ .close
+│ ➸ .mute 
+│ ➸ .unmute
+│ ➸ .delete 
+│ ➸ .kick 
+│ ➸ .warnings 
+│ ➸ .warn 
+│ ➸ .antilink
+│ ➸ .antibadword
+│ ➸ .clear
+│ ➸ .tag 
+│ ➸ .tagall
+│ ➸ .resetlink
+│ ➸ .welcome 
+│ ➸ .goodbye 
+╰─✦
+
+╭─✦〔 🌐 *General* 〕
+│ ➸ .menu
+│ ➸ .ping
+│ ➸ .alive
+│ ➸ .tts 
+│ ➸ .owner
+│ ➸ .joke
+│ ➸ .quote
+│ ➸ .fact
+│ ➸ .weather 
+│ ➸ .news
+│ ➸ .lyrics 
+│ ➸ .8ball 
+│ ➸ .groupinfo
+│ ➸ .admins
+│ ➸ .vv
+│ ➸ .vv2
+│ ➸ .ss 
+│ ➸ .jid
+╰─✦
+
+╭─✦〔 🔒 *Owner* 〕
+│ ➸ .statusreact
+│ ➸ .mode
+│ ➸ .anticall
+│ ➸ .statusview
+│ ➸ .statusreply
+│ ➸ .statusmsg
+│ ➸ .autotype
+│ ➸ .autorecord
+│ ➸ .autorecordtype
+│ ➸ .clearsession
+│ ➸ .antidelete
+│ ➸ .antiedit
+│ ➸ .cleartmp
+│ ➸ .setpp 
+│ ➸ .getpp
+│ ➸ .save
+│ ➸ .setprefix
+│ ➸ .update
+│ ➸ .autobio
+│ ➸ .autoreact
+╰─✦
+
+╭─✦〔 🎨 *Photo* 〕
+│ ➸ .blur 
+│ ➸ .simage 
+│ ➸ .sticker 
+│ ➸ .tgsticker 
+│ ➸ .meme
+│ ➸ .take 
+│ ➸ .emojimix
+╰─✦
+
+╭─✦〔 ✝️ *Religion* 〕
+│ ➸ .bible
+│ ➸ .quran
+│ ➸ .catholic
+╰─✦
+
+╭─✦〔 💻 *Tools* 〕
+│ ➸ .trim
+│ ➸ .reportbug
+│ ➸ .ngl
+│ ➸ .tomp3
+│ ➸ .script
+│ ➸ .repo
+│ ➸ .vcf
+╰─✦
+
+╭─✦〔 🔤 *Textmaker* 〕
+│ ➸ .metallic 
+│ ➸ .ice 
+│ ➸ .snow 
+│ ➸ .impressive 
+│ ➸ .matrix 
+│ ➸ .light 
+│ ➸ .neon 
+│ ➸ .devil 
+│ ➸ .purple 
+│ ➸ .thunder 
+│ ➸ .leaves 
+│ ➸ .1917 
+│ ➸ .arena 
+│ ➸ .hacker 
+│ ➸ .sand 
+│ ➸ .blackpink 
+│ ➸ .glitch 
+│ ➸ .fire 
+╰─✦
+
+> *© ᴘᴏᴡᴇʀᴇᴅ ʙʏ 𝚀𝚄𝙴𝙴𝙽 𝚁𝙸𝙰𝙼*
